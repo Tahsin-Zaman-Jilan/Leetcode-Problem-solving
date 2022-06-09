0039cb4e488f5e72908e9876349a546be4121608
Tahsin-Zaman-Jilan <77837621+Tahsin-Zaman-Jilan@users.noreply.github.com>
2022-06-09 20:55:22 +0600

    Time: 3 ms (50.43%), Space: 7.5 MB (97.93%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        
+        priority_queue<int>pq;
+        
+        int len=stones.size();
+      for(int i=0;i<len;i++){
+          pq.push(stones[i]);
+      }
+       int  maxm=0;
+        for(int i=0;i<len-1;i++){
+            
+            int ans=pq.top();
+            cout<<ans<<endl;
+            pq.pop();
+            int ans1=pq.top();
+             pq.pop();
+            cout<<ans1<<endl;
+            maxm=abs(ans-ans1);
+            pq.push(maxm);
+            
+        }
+        
+        if(stones.size()==1){
+            return stones[0];
+        }
+        return pq.top();
+        
+        
+    }
+};
